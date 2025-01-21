ef04026fe6e3040570242546dc8aa84a165214ad
jubaerahmedtamim <jubaerahmedtamim2014@gmail.com>
2025-01-21 23:57:52 +0600

    manage property by host done without actions button

--- src/Pages/Dashboard/Host/ManageProperties/ManageProperties.jsx
+++ src/Pages/Dashboard/Host/ManageProperties/ManageProperties.jsx
@@ -9,10 +9,11 @@ const ManageProperties = () => {
     const { user } = useAuth();
     const axiosSecure = useAxiosSecure();
 
-    const { data: properties =[] } = useQuery({
+    const { data: properties = [] } = useQuery({
         queryKey: ['properties'],
         queryFn: async () => {
             const res = await axiosSecure.get(`/properties?email=${user?.email}`)
+            
             return res.data;
         }
     })
@@ -23,8 +24,37 @@ const ManageProperties = () => {
                 <title>Manage Properties</title>
             </Helmet>
             <SectionTitle heading={'manage property'} subHeading={'Manage your property'}></SectionTitle>
-            <div>
-                {properties.length}
+            <div className=" w-full md:max-w-4xl mx-auto">
+                <h2 className="text-xl">Your property: {properties.length}</h2>
+                <div className="overflow-x-auto">
+                    <table className="table">
+                        {/* head */}
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Property Id</th>
+                                <th>Type</th>
+                                <th>Monthly rent</th>
+                                <th>Availability</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {/* row 1 */}
+                            {
+                                properties.map((property, index) => <tr key={property._id} className="hover">
+                                    <th>{index + 1}</th>
+                                    <td>{property.propertyId}</td>
+                                    <td>{property.type}</td>
+                                    <td>{property.monthlyRent}</td>
+                                    <td>{property.isAvailable ? "Available" : "Booked"}</td>
+                                    <td></td>
+                                </tr>)
+                            }
+                            
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     );
